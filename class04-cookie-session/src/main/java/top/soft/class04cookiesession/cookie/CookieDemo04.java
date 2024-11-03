d368c9e70e089f4ec3c167f9cbc393664a46af30
Talula52 <13912718233@163.com>
2024-11-03 23:19:41 +0800

    Add files via upload

---
+++ class04-cookie-session/src/main/java/top/soft/class04cookiesession/cookie/CookieDemo04.java
@@ -0,0 +1,35 @@
+package top.soft.class04cookiesession.cookie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author msy13
+ * @description: TODO
+ * @date 2024/10/19 14:07
+ */
+@WebServlet("/cookieDemo04")
+public class CookieDemo04 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+        for(Cookie cookie:cookies){
+            String name = cookie.getName();
+            if ("chineseName".equals(name)) {
+                String value = cookie.getValue();
+                String decode = URLDecoder.decode(value, StandardCharsets.UTF_8);
+                System.out.println("用户名："+ decode);
+                break;
+            }
+        }
+
+    }
+}
